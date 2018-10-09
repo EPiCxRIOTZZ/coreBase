b88982414196bdf1e7c14b4c53bd225ce4c89b1d
Lakr Sakura <2333OwO@gmail.com>
2018-10-09 23:40:01 +0800

    Fixed Always Ask For Password in 10.14

--- OneMonkey.sh
+++ OneMonkey.sh
@@ -72,93 +72,93 @@ fi
 echo "[!] Homebrew has compeleted. Now, let's have it in root."
 
 echo -e "$rootpassvar\n" | sudo -S echo "Lets install pip first..."
-sudo easy_install pip
-sudo rm -rf ~/OneMonkey
-sudo mkdir ~/OneMonkey
+echo -e "$rootpassvar\n" | sudo -S  easy_install pip
+echo -e "$rootpassvar\n" | sudo -S  rm -rf ~/OneMonkey
+echo -e "$rootpassvar\n" | sudo -S  mkdir ~/OneMonkey
 cd ~/OneMonkey
 
-sudo echo "[Info] Installing ldid2...... for some stupid jb like ......."
-sudo wget https://github.com/Co2333/coreBase/blob/master/ldid2?raw=true ./
-sudo cp ./ldid2 /usr/local/bin/ldid2
-sudo chmod +x /usr/local/bin/ldid2
-sudo echo "[Info] Installing THEOS..."
-sudo rm -rf /opt/THEOS
-sudo git clone --recursive https://github.com/theos/theos.git /opt/theos
-sudo chown $(id -u):$(id -g) /opt/theos
-sudo echo "[Info] Adding some shit to your computer... export THEOS"
+echo -e "$rootpassvar\n" | sudo -S  echo "[Info] Installing ldid2...... for some stupid jb like ......."
+echo -e "$rootpassvar\n" | sudo -S  wget https://github.com/Co2333/coreBase/blob/master/ldid2?raw=true ./
+echo -e "$rootpassvar\n" | sudo -S  cp ./ldid2 /usr/local/bin/ldid2
+echo -e "$rootpassvar\n" | sudo -S  chmod +x /usr/local/bin/ldid2
+echo -e "$rootpassvar\n" | sudo -S  echo "[Info] Installing THEOS..."
+echo -e "$rootpassvar\n" | sudo -S  rm -rf /opt/THEOS
+echo -e "$rootpassvar\n" | sudo -S  git clone --recursive https://github.com/theos/theos.git /opt/theos
+echo -e "$rootpassvar\n" | sudo -S  chown $(id -u):$(id -g) /opt/theos
+echo -e "$rootpassvar\n" | sudo -S  echo "[Info] Adding some shit to your computer... export THEOS"
 export THEOS=/opt/theos
 export PATH=/opt/theos/bin/:$PATH
 source ~/.bash_profile
 
-sudo echo "[Info] Starting install this Monkey..."
-sudo git clone https://github.com/AloneMonkey/MonkeyDev.git
-sudo chmod +x ./MonkeyDev/bin/md-install
-sudo chmod +x ./MonkeyDev/bin/md-uninstall
-sudo rm -rf /opt/MonkeyDev
-sudo ./MonkeyDev/bin/md-uninstall
-sudo ./MonkeyDev/bin/md-install
+echo -e "$rootpassvar\n" | sudo -S  echo "[Info] Starting install this Monkey..."
+echo -e "$rootpassvar\n" | sudo -S  git clone https://github.com/AloneMonkey/MonkeyDev.git
+echo -e "$rootpassvar\n" | sudo -S  chmod +x ./MonkeyDev/bin/md-install
+echo -e "$rootpassvar\n" | sudo -S  chmod +x ./MonkeyDev/bin/md-uninstall
+echo -e "$rootpassvar\n" | sudo -S  rm -rf /opt/MonkeyDev
+echo -e "$rootpassvar\n" | sudo -S  ./MonkeyDev/bin/md-uninstall
+echo -e "$rootpassvar\n" | sudo -S  ./MonkeyDev/bin/md-install
 
-sudo echo "[Info] Starting install frida..."
-sudo pip install frida-tools --ignore-installed six wcwidth
-sudo git clone https://github.com/AloneMonkey/frida-ios-dump.git
-sudo pip install -r ./frida-ios-dump/requirements.txt  --upgrade
-sudo echo "[!] dump.py will be install to /usr/local/bin and named fridaDP.py"
-sudo cp ./frida-ios-dump/process.sh /usr/local/bin/process.sh
-sudo cp ./frida-ios-dump/dump.js /usr/local/bin/dump.js
-sudo cp ./frida-ios-dump/dump.py /usr/local/bin/fridaDP.py
+echo -e "$rootpassvar\n" | sudo -S  echo "[Info] Starting install frida..."
+echo -e "$rootpassvar\n" | sudo -S  pip install frida-tools --ignore-installed six wcwidth
+echo -e "$rootpassvar\n" | sudo -S  git clone https://github.com/AloneMonkey/frida-ios-dump.git
+echo -e "$rootpassvar\n" | sudo -S  pip install -r ./frida-ios-dump/requirements.txt  --upgrade
+echo -e "$rootpassvar\n" | sudo -S  echo "[!] dump.py will be install to /usr/local/bin and named fridaDP.py"
+echo -e "$rootpassvar\n" | sudo -S  cp ./frida-ios-dump/process.sh /usr/local/bin/process.sh
+echo -e "$rootpassvar\n" | sudo -S  cp ./frida-ios-dump/dump.js /usr/local/bin/dump.js
+echo -e "$rootpassvar\n" | sudo -S  cp ./frida-ios-dump/dump.py /usr/local/bin/fridaDP.py
 
-sudo echo "[Info] Starting install jtool and joker tool by additional..."
+echo -e "$rootpassvar\n" | sudo -S  echo "[Info] Starting install jtool and joker tool by additional..."
 cd ~/OneMonkey
-sudo wget http://www.newosxbook.com/tools/jtool.tar
-sudo wget http://www.newosxbook.com/tools/joker.tar
-sudo tar -xvf jtool.tar
-sudo tar -xvf joker.tar
-sudo cp ./jtool /usr/local/bin/jtool
-sudo cp ./joker.universal /usr/local/bin/joker
-sudo chmod +x /usr/local/bin/jtool
-sudo chmod +x /usr/local/bin/joker
+echo -e "$rootpassvar\n" | sudo -S  wget http://www.newosxbook.com/tools/jtool.tar
+echo -e "$rootpassvar\n" | sudo -S  wget http://www.newosxbook.com/tools/joker.tar
+echo -e "$rootpassvar\n" | sudo -S  tar -xvf jtool.tar
+echo -e "$rootpassvar\n" | sudo -S  tar -xvf joker.tar
+echo -e "$rootpassvar\n" | sudo -S  cp ./jtool /usr/local/bin/jtool
+echo -e "$rootpassvar\n" | sudo -S  cp ./joker.universal /usr/local/bin/joker
+echo -e "$rootpassvar\n" | sudo -S  chmod +x /usr/local/bin/jtool
+echo -e "$rootpassvar\n" | sudo -S  chmod +x /usr/local/bin/joker
 
-sudo echo "[Info] Install class-dump also by additional..."
-sudo wget http://stevenygard.com/download/class-dump-3.5.tar.gz
-sudo tar -xzf class-dump-3.5.tar.gz
-sudo cp ./class-dump-3.5/class-dump /usr/local/bin/class-dump
-sudo chmod +x /usr/local/bin/class-dump
+echo -e "$rootpassvar\n" | sudo -S  echo "[Info] Install class-dump also by additional..."
+echo -e "$rootpassvar\n" | sudo -S  wget http://stevenygard.com/download/class-dump-3.5.tar.gz
+echo -e "$rootpassvar\n" | sudo -S  tar -xzf class-dump-3.5.tar.gz
+echo -e "$rootpassvar\n" | sudo -S  cp ./class-dump-3.5/class-dump /usr/local/bin/class-dump
+echo -e "$rootpassvar\n" | sudo -S  chmod +x /usr/local/bin/class-dump
 
-sudo echo "[Info] Install jtool2 for future use..."
-sudo wget https://github.com/Co2333/coreBase/raw/master/jtool2
-sudo cp ./jtool2 /usr/local/bin/jtool2
-sudo chmod +x /usr/local/bin/jtool2
+echo -e "$rootpassvar\n" | sudo -S  echo "[Info] Install jtool2 for future use..."
+echo -e "$rootpassvar\n" | sudo -S  wget https://github.com/Co2333/coreBase/raw/master/jtool2
+echo -e "$rootpassvar\n" | sudo -S  cp ./jtool2 /usr/local/bin/jtool2
+echo -e "$rootpassvar\n" | sudo -S  chmod +x /usr/local/bin/jtool2
 
 
-# sudo echo "[!] Tell me your iOS ssh password. I will insert it to this py."
+# echo -e "$rootpassvar\n" | sudo -S  echo "[!] Tell me your iOS ssh password. I will insert it to this py."
 
 # while true; do
 # read -sp '[*] Password: ' passvar
 # read -p "[*] Is this your password? $passvar  [Y(es)/N(o)]:" yn
 # case $yn in
 # [Yy]* ) break;;
-# [Nn]* ) sudo echo "[Error] Fine, stupid. Let's do it again.";;
+# [Nn]* ) echo -e "$rootpassvar\n" | sudo -S  echo "[Error] Fine, stupid. Let's do it again.";;
 # * ) echo "[Error] Fuck U! Please answer y or n. Let's do it again.";;
 # esac
 # done
 # export SEDTMP=s/alpine/$passvar/g
-# sudo sed -i '' -e $SEDTMP /usr/local/bin/fridaDP.py
+# echo -e "$rootpassvar\n" | sudo -S  sed -i '' -e $SEDTMP /usr/local/bin/fridaDP.py
 
-# sudo echo "[!] Tell me your iOS ssh address. I will insert it to this py."
+# echo -e "$rootpassvar\n" | sudo -S  echo "[!] Tell me your iOS ssh address. I will insert it to this py."
 # while true; do
 # read -sp '[*] iP Address: ' passvar
 # read -p "[*] Is this your iP Address? $passvar  [Y(es)/N(o)]:" yn
 # case $yn in
 # [Yy]* ) break;;
-# [Nn]* ) sudo echo "[Error] Fine, stupid. Let's do it again.";;
+# [Nn]* ) echo -e "$rootpassvar\n" | sudo -S  echo "[Error] Fine, stupid. Let's do it again.";;
 # * ) echo "[Error] Fuck U! Please answer y or n. Let's do it again.";;
 # esac
 # done
 # export SEDTMP=s/localhost/$passvar/g
-# sudo sed -i '' -e $SEDTMP /usr/local/bin/fridaDP.py
+# echo -e "$rootpassvar\n" | sudo -S  sed -i '' -e $SEDTMP /usr/local/bin/fridaDP.py
 
-sudo echo "[*] Done? Yes. Removing files we don't need anymore."
-sudo rm -rf ~/OneMonkey
+echo -e "$rootpassvar\n" | sudo -S  echo "[*] Done? Yes. Removing files we don't need anymore."
+echo -e "$rootpassvar\n" | sudo -S  rm -rf ~/OneMonkey
 
 echo "[Info] Compeleted install."
 echo "MonkeyDev by AloneMonkey"
